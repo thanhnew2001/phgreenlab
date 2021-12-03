1b58c3d7e4434c81dd3d0eabed3aef60ad76c687
huongnguyen3006 <83159726+huongnguyen3006@users.noreply.github.com>
2021-12-03 09:44:48 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import {
   BrowserRouter as Router, Routes,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+
 import Labs from "./components/Labs";
 import Home from "./components/Home"
-
+import DeviceDashBoard from './components/DeviceDashBoard';
+import Sensor from './components/Sensor';
 
 function App() {
  return (
         <Router >
-          
+          <Home />
           <div>
           <Routes>
              
-             <Route path="/" caseSensitive={false} element={ <Home />} />
+             <Route path="/"  />
              </Routes>
        
            
@@ -25,7 +29,16 @@ function App() {
              
               <Route path="/labs" caseSensitive={false} element={ <Labs />} />
               </Routes>
-        
+              <Routes>
+             
+             <Route path="/devices" caseSensitive={false} element={ <DeviceDashBoard />} />
+             </Routes>
+            
+
+             <Routes>
+             <Route path="/sensors" caseSensitive={false} element={ <Sensor />} />
+             </Routes>
+
           </div>
           </Router>
    
