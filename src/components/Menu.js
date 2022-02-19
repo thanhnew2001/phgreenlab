ee5af4340e3696acb1f0ecdca8c9eeaa115f0799
huongng0201 <83159726+huongnguyen3006@users.noreply.github.com>
2022-02-19 11:05:15 +0700

    cm

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -21,13 +21,16 @@ export default function Menu() {
         <div class="collapse navbar-collapse" id="collapsibleNavbar" >
           <ul class="navbar-nav">
             <li class="nav-item">
-            <a style={{ color: '#388e3c', fontSize: 30 }} class="nav-link" href="/"> <FontAwesomeIcon icon={faHome} /> </a>
+            <a style={{ color: '#388e3c', fontSize: 25 }} class="nav-link" href="/"> <FontAwesomeIcon icon={faHome} /> </a>
             </li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <li class="nav-item">
-              <a style={{ color: '#388e3c', fontSize: 30 }} class="nav-link" href="/devices">OMS</a>
+              <a style={{ color: '#388e3c', fontSize: 25 }} class="nav-link" href="/devices">OMS</a>
             </li>
             <li class="nav-item">
-              <a style={{ color: '#388e3c', fontSize: 30 }} class="nav-link" href="/devicesform">Device</a>
+              <a style={{ color: '#388e3c', fontSize: 25 }} class="nav-link" href="/devicesform">Device</a>
+            </li>
+            <li class="nav-item">
+              <a style={{ color: '#388e3c', fontSize: 25 }} class="nav-link" href="/users">User</a>
             </li>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -43,7 +46,7 @@ export default function Menu() {
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <div class="nav-item dropdown">
               <li  >
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" style={{ float: 'right', color: '#388e3c', fontSize: 30 }}>smartpH</a>
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" style={{ float: 'right', color: '#388e3c', fontSize: 25 }}>smartpH</a>
                 <ul class="dropdown-menu  " >
                   <li><a class="dropdown-item" href="/signin">Sign In</a></li>
                   <li><a class="dropdown-item" href="/signup">Sign Up</a></li>
